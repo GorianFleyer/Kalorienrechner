8a791b70a3edacc36043422ecb59da615edd6657
GorianFleyer <floriangeyer@mail.de>
2020-02-24 21:22:54 +0100

    Base

---
+++ src/com/fg/Calculator.java
@@ -0,0 +1,23 @@
+package com.fg;
+
+public class Calculator
+{
+
+
+    // Relation: x Gram * y Calories (per 100g) = xy Gram Calories / 100
+
+    public static double GramCalories( double gram, double calories)
+    {
+        return gram * calories / 100;
+    }
+
+    // Reach the ratio of Calories per 100g
+
+    public static double Ratio(double gram, double calories)
+    {
+        return calories * 100 / gram;
+    }
+
+    public static double
+
+}
